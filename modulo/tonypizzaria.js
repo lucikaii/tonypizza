b7a4f425721b15a03db2aef1dde222cf4f169e4a
Bragakaii <bragaluciano.nan@gmail.com>
2023-11-24 12:24:29 -0300

    kkkkk

---
+++ modulo/tonypizzaria.js
@@ -0,0 +1,168 @@
+var pizzaria = {
+
+    contas:
+    [
+        {
+            nome: "Kainan",
+            email: "kai.luciano@gmail.com",
+            senha: "kaikai123",
+            celular: "11955964264",
+            endereco: [
+                {
+                    estado: "São Paulo",
+                    cidade: "Jandira",
+                    bairro: "Parque Santa Agustinha",
+                    complemento: "Rua Tenessee, 199"
+                }
+            ]
+            
+        },
+        {
+            nome: "Alice",
+            email: "licezeurgo@gmail.com",
+            senha: "licefuturatavares",
+            celular: "11940028922",
+            endereco: [
+                {
+                    estado: "São Paulo",
+                    cidade: "Jandira",
+                    bairro: "Parque Santa Tere",
+                    complemento: "Rua Amadeu, 45"
+                }
+            ]
+            
+        },
+        {
+            nome: "Thamires",
+            email: "bangtan20142004@gmail.com",
+            senha: "odeiooprofbruno",
+            celular: "11988467270",
+            endereco: [
+                {
+                    estado: "São Paulo",
+                    cidade: "Carapicuiba",
+                    bairro: "Fim do mundo",
+                    complemento: "Rua dos Loucos, 69"
+                }
+            ]
+            
+        },
+        {
+            nome: "Luana",
+            email: "luanamaga18@gmail.com",
+            senha: "eusobotoapirocanaminhabundaquandootiosan",
+            celular: "11966666666",
+            endereco: [
+                {
+                    estado: "São Paulo",
+                    cidade: "Jandira",
+                    bairro: "Jardim Mazé",
+                    complemento: "Rua da Luana, 666"
+                }
+            ]
+            
+        },
+        {
+            nome: "Maria Luiza",
+            email: "malubalieiro@gmail.com",
+            senha: "taylorlindona",
+            celular: "11978423674",
+            endereco: [
+                {
+                    estado: "São Paulo",
+                    cidade: "Barueri",
+                    bairro: "Jardim Belval",
+                    complemento: "Rua do Saci Pererê, 44"
+                }
+            ]
+            
+        },
+        {
+            nome: "Suzane",
+            email: "ssusuarcanjo@gmail.com",
+            senha: "pererecasuicida",
+            celular: "11979782058",
+            endereco: [
+                {
+                    estado: "São Paulo",
+                    cidade: "Barueri",
+                    bairro: "Jardim Belval",
+                    complemento: "Rua do Inspiri, 10"
+                }
+            ]
+            
+        },
+        {
+            nome: "Jhenifer",
+            email: "jhenibatista@gmail.com",
+            senha: "amooplhaco",
+            celular: "11955747869",
+            endereco: [
+                {
+                    estado: "São Paulo",
+                    cidade: "Cotia",
+                    bairro: "Parque",
+                    complemento: "Rua Osasco, 79"
+                }
+            ]
+            
+        },
+        {
+            nome: "Nathalia Kawakami",
+            email: "kawaka@gmail.com",
+            senha: "soobim",
+            celular: "11969696969",
+            endereco: [
+                {
+                    estado: "São Paulo",
+                    cidade: "Jandira",
+                    bairro: "Bairro dos Predinhos",
+                    complemento: "Rua dos Predinhos, 79"
+                }
+            ]
+            
+        },
+        {
+            nome: "Bernardo",
+            email: "bernardinho@gmail.com",
+            senha: "lanadelrey",
+            celular: "11969696969",
+            endereco: [
+                {
+                    estado: "Bahia",
+                    cidade: "Cidade do Bernardo",
+                    bairro: "Bairro da Pepsi",
+                    complemento: "Rua do Summertime Sadness, 69"
+                }
+            ]
+            
+        },
+       
+    ],
+
+    categoria: 
+    [
+        {
+            nome: "Pizzas Salgadas"
+        },
+        {
+            nome: "Pizzas Doces"
+        },
+        {
+            nome: "Bebidas"
+        },
+        {
+            nome: "Sobremesas"
+        }
+
+    ],
+
+    pizzaSalgadas:
+    [
+        {
+            nome: "Muçarela",
+            preco: "28,00",
+
+        }
+    ]
+}
